14829c0d93e6f9cb9c7c2591b76489bcbf27156a
Fabrice Aneche <akh@nobugware.com>
2013-08-06 07:42:04 +0200

    added mutex and Suggest
    added suggest with C functions to browse inside damn ***
    added a lock to avoid concurrency problem on C side

--- hunspellgo.go
+++ hunspellgo.go
@@ -1,49 +1,105 @@
 package hunspellgo
 
-// #cgo LDFLAGS: -lhunspell
+import (
+	"runtime"
+	"sync"
+	"unsafe"
+)
+
+// #cgo linux LDFLAGS: -lhunspell
+// #cgo darwin LDFLAGS: -lhunspell-1.3 -L/opt/local/lib
+// #cgo darwin CFLAGS: -I/opt/local/include
 // #include <stdlib.h>
 // #include <stdio.h>
 // #include <hunspell/hunspell.h>
-import  "C"
-import  "unsafe"
-import  "fmt"
-import  "runtime"
+import "C"
 
-func Hunspell(affpath string, dpath string) (*Hunhandle) {
+type Hunhandle struct {
+	handle *C.Hunhandle
+	lock   *sync.Mutex
+}
+
+func Hunspell(affpath string, dpath string) *Hunhandle {
 	affpathcs := C.CString(affpath)
 	defer C.free(unsafe.Pointer(affpathcs))
 	dpathcs := C.CString(dpath)
 	defer C.free(unsafe.Pointer(dpathcs))
-  h := &Hunhandle{nil}
+	h := &Hunhandle{lock: new(sync.Mutex)}
 	h.handle = C.Hunspell_create(affpathcs, dpathcs)
-  runtime.SetFinalizer(h, func(handle *Hunhandle) {
-    C.Hunspell_destroy(handle.handle)
-    h.handle = nil 
-  })
+	runtime.SetFinalizer(h, func(handle *Hunhandle) {
+		C.Hunspell_destroy(handle.handle)
+		h.handle = nil
+	})
 	return h
 }
 
-func (handle *Hunhandle) Suggest(word string) {
+func sptr(p uintptr) *C.char {
+	return *(**C.char)(unsafe.Pointer(p))
+}
+
+func CStrings(x **C.char, len int) []string {
+	var s []string
+
+	p := uintptr(unsafe.Pointer(x))
+	for i := 0; i < len; i++ {
+		p += unsafe.Sizeof(uintptr(0))
+		if sptr(p) == nil {
+			break
+		}
+		s = append(s, C.GoString(sptr(p)))
+	}
+
+	// for p := uintptr(unsafe.Pointer(x)); sptr(p) != nil; p += unsafe.Sizeof(uintptr(0)) {
+	// 	s = append(s, C.GoString(sptr(p)))
+	// }
+	return s
+}
+
+func (handle *Hunhandle) Suggest(word string) []string {
 	wordcs := C.CString(word)
 	defer C.free(unsafe.Pointer(wordcs))
+	var carray **C.char
+	var length C.int
+	handle.lock.Lock()
+	length = C.Hunspell_suggest(handle.handle, &carray, wordcs)
+	handle.lock.Unlock()
 
-  var carray **C.char
-  length := C.Hunspell_suggest(handle.handle, &carray, wordcs)
-  fmt.Println(length)
-  //C.printf("%s", &carray)
-  C.Hunspell_free_list(handle.handle, &carray, length)
+	if int(length) == 1 {
+		return []string{}
+	}
+	c_strings := CStrings(carray, int(length))
+
+	C.Hunspell_free_list(handle.handle, &carray, length)
+	return c_strings
+}
+
+func (handle *Hunhandle) Stem(word string) []string {
+	wordcs := C.CString(word)
+	defer C.free(unsafe.Pointer(wordcs))
+	var carray **C.char
+	var length C.int
+	handle.lock.Lock()
+	length = C.Hunspell_stem(handle.handle, &carray, wordcs)
+	handle.lock.Unlock()
+
+	if int(length) == 1 {
+		return []string{}
+	}
+	strings := CStrings(carray, int(length))
+
+	C.Hunspell_free_list(handle.handle, &carray, length)
+	return strings
 }
 
 func (handle *Hunhandle) Spell(word string) bool {
-  wordcs := C.CString(word)
-  defer C.free(unsafe.Pointer(wordcs))
-  res := C.Hunspell_spell(handle.handle, wordcs)
-  if int(res) == 0 {
-    return false
-  }
-  return true
-}
+	wordcs := C.CString(word)
+	defer C.free(unsafe.Pointer(wordcs))
+	handle.lock.Lock()
+	res := C.Hunspell_spell(handle.handle, wordcs)
+	handle.lock.Unlock()
 
-type Hunhandle struct {
-	handle *C.Hunhandle
+	if int(res) == 0 {
+		return false
+	}
+	return true
 }
